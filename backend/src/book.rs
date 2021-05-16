7d23e0472e46cade53ca2c12d28d489152cb7bb4
Rickard Andersson <dunklas@gmail.com>
2021-05-16 15:08:39 +0200

    Implement handlers that store/get from db

--- backend/src/book.rs
+++ backend/src/book.rs
@@ -1,7 +1,17 @@
 use serde::{Serialize, Deserialize};
+use diesel::{Queryable, Insertable};
+use crate::schema::books;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Debug, Queryable)]
 pub struct Book {
+    pub id: i32,
+    pub title: String,
+    pub author: String,
+}
+
+#[derive(Debug, Deserialize, Insertable)]
+#[table_name = "books"]
+pub struct NewBook {
     pub title: String,
     pub author: String,
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,30 +1,44 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate diesel_migrations;
+#[macro_use] extern crate diesel;
+
+mod book;
+use book::{NewBook, Book};
+mod schema;
 
 use rocket::Rocket;
 use rocket::fairing::AdHoc;
 use rocket_contrib::json::Json;
-use rocket_contrib::databases::{database, diesel};
-
-mod book;
-use book::Book;
+use rocket_contrib::databases::{database, diesel as rocket_contrib_diesel};
+use rocket_contrib_diesel::RunQueryDsl;
 
 #[database("book_db")]
-struct BookDb(diesel::PgConnection);
+struct BookDb(rocket_contrib_diesel::PgConnection);
+
+fn load_from_db(conn: &diesel::PgConnection) -> Vec<Book> {
+    let books = schema::books::table.load::<Book>(conn)
+        .expect("Error reading books");
+    books
+}
+
+fn insert_book(conn: &diesel::PgConnection, book: &NewBook) -> Book {
+    let book = diesel::insert_into(schema::books::table)
+        .values(book)
+        .get_result::<Book>(conn)
+        .expect("Error saving new book");
+    book
+}
 
 #[post("/", format = "application/json", data = "<book>")]
-fn create_book(book: Json<Book>) -> Json<Book> {
-    let book = Book{
-        title: "Call of Cthulu".to_owned(),
-        author: "H.P. Lovecraft".to_owned(),
-    };
-    Json(book)
+fn create_book(book: Json<NewBook>, db: BookDb) -> Json<Book> {
+    Json(insert_book(&*db, &book.into_inner()))
 }
 
 #[get("/", format = "application/json")]
-fn get_books() -> Json<Vec<Book>> {
-    Json(Vec::new())
+fn get_books(db: BookDb) -> Json<Vec<Book>> {
+    let books = load_from_db(&*db);
+    Json(books)
 }
 
 fn run_db_migrations(rocket: Rocket) -> Result<Rocket, Rocket> {
