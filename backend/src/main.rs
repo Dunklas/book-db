1bd7fcfb6d56099cd4d30e64acc91b09d3af4546
Dunklas <dunklas@gmail.com>
2021-05-20 20:38:29 +0200

    Add delete endpoint (#3)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -11,36 +11,38 @@ use rocket::fairing::AdHoc;
 use rocket_contrib::json::Json;
 use rocket_contrib::databases::{database, diesel as rocket_contrib_diesel};
 use rocket_contrib_diesel::RunQueryDsl;
+use diesel::prelude::*;
+use schema::books::dsl::*;
 
 #[database("book_db")]
 struct BookDb(rocket_contrib_diesel::PgConnection);
 
-fn load_from_db(conn: &diesel::PgConnection) -> Vec<Book> {
-    let books = schema::books::table.load::<Book>(conn)
-        .expect("Error reading books");
-    books
-}
-
-fn insert_book(conn: &diesel::PgConnection, book: &NewBook) -> Book {
-    let book = diesel::insert_into(schema::books::table)
-        .values(book)
-        .get_result::<Book>(conn)
-        .expect("Error saving new book");
-    book
-}
-
 #[post("/", format = "application/json", data = "<book>")]
 async fn create_book(book: Json<NewBook>, db: BookDb) -> Json<Book> {
     db.run(|connection| {
-        Json(insert_book(&connection, &book.into_inner()))
+        let result = diesel::insert_into(books)
+            .values(book.into_inner())
+            .get_result::<Book>(connection)
+            .expect("Error saving new book");
+        Json(result)
     }).await
 }
 
 #[get("/", format = "application/json")]
 async fn get_books(db: BookDb) -> Json<Vec<Book>> {
     db.run(|connection| {
-        Json(load_from_db(&connection))
+        Json(books.load::<Book>(connection)
+            .expect("Error reading books"))
     }).await
+}
+
+#[delete("/<book_id>")]
+async fn delete_book(book_id: i32, db: BookDb) {
+    db.run(move |connection| {
+        diesel::delete(books.filter(id.eq(book_id)))
+            .execute(connection)
+            .expect("Error deleting book");
+    }).await;
 }
 
 async fn run_db_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
@@ -65,7 +67,9 @@ async fn main() {
         .mount("/books", routes![
             create_book,
             get_books,
+            delete_book,
         ])
         .launch()
-        .await;
+        .await
+        .unwrap();
 }
